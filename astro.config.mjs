8a36c309ca7e4320e84d194d49190ac53c5dce12
devEducaua <schokito4@gmail.com>
2025-09-29 01:50:41 -0300

    fix: path on astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site: "https://deveducaua.github.io/www/",
     outDir: "docs",
-    base: "/www/",
+    // base: "/www/",
 
     build: {
         pageUrlFormat: "directory",
