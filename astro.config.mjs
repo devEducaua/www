6f214aafb82acdd431ede12c96c23f92cc3deed4
devEducaua <schokito4@gmail.com>
2025-09-29 17:16:10 -0300

    feat: disable md code blocks highlights

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
     site: "https://deveducaua.github.io/www",
     outDir: "docs",
@@ -14,5 +12,9 @@ export default defineConfig({
 
     devToolbar: {
         enabled: false
+    },
+
+    markdown: {
+        syntaxHighlighting: false
     }
 });
