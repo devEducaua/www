ca618c2888e1b372bd17363bf82f202b0ab6a704
devEducaua <schokito4@gmail.com>
2025-09-29 01:26:19 -0300

    fix: astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://deveducaua.github.io/",
-    base: "/docs",
+    site: "https://deveducaua.github.io",
 
     build: {
         pageUrlFormat: "directory",
